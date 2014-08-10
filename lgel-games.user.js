92da631fac52b7ce020315a5476125b2a4e7faa1
Matthieu Dumont <jerska@live.fr>
2014-08-11 00:31:03 +0200

    First lgel version

---
+++ lgel-games.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// // @name        lgel-games
+// // @namespace   http://www.github.com/Jerskouille/lgel
+// // @description lgel
+// // @include     *loups-garous-en-ligne.com/room
+// // @version     0.0.1
+// // @grant       none
+// // ==/UserScript==
